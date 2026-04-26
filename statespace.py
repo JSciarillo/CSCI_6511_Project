6f07e6b072c606cbaf1209f52152e969a5e16e2a
JSciarillo <jasminesciarillo@yahoo.com>
2026-04-25 21:49:01 -0400

    Milestone 2

---
+++ statespace.py
@@ -0,0 +1,9 @@
+S = {B,H,D,P,T}
+
+B = {0,c1,c2,c3,....cn}^{100}
+H = {h1,h2,..hn} hand of each player, h<=k (deck of cards)
+D = the card draw pile, D<=k (the remaining pile of cards)
+P = {p1,p2,p3,.....pn} #the sequence count vector
+T = {1, 2 , n}  #index of current player
+
+# Implementing the state space, to generate successor state, given actions
